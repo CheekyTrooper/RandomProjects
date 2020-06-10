e7cb9a5269262f2070e003de905af4208601c98e
CheekyTrooper <Jasonreynolds96@googlemail.com>
2020-06-10 13:09:20 +0100

    ReverseAString Completed
    ReverseAString has been completed with comments.

---
+++ ReverseAString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Continious loop to keep console open and to re-iterate once a string has been converted.
+            while (true)
+            {
+                Console.WriteLine("Please input your string to reverse.");
+
+                //Read the input text in the console and pass to the originalString variable.
+                string originalString = Console.ReadLine();
+
+                //Check if the originalString is populated, if not the user has pressed enter without populating.
+                if (originalString != "")
+                {
+                    //Read out the string returned from the ReverseString method.
+                    Console.WriteLine($"Your reverse string is: \n{ReverseString(originalString)}");
+                }
+                else
+                {
+                    //If the user has not 
+                    Console.WriteLine("No text input");
+                }
+                //To add space for readability.
+                Console.WriteLine("\n");
+            }
+        }
+
+        private static string ReverseString (string s)
+        {
+            //Split out the string into a character array eg.{test -> t,e,s,t}
+            char[] charArray = s.ToCharArray();
+            //Reverse the charArray and save it back to charArray eg.{t,e,s,t = t,s,e,t}
+            Array.Reverse(charArray);
+            //return the new combined string
+            return new string(charArray);
+        }
+    }
+}
